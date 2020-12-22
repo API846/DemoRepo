a439383c3147e547301f49fa227cb1cd2764115e
ChandiniAlekhya <chandini.gollapudi@apiero.com>
2020-12-22 10:23:13 +0530

    I am First Commit

---
+++ Reverse.java
@@ -0,0 +1,16 @@
+public class Reverse {
+
+	public static void main(String[] args) {
+		
+		String a="Hello World";
+		int c=a.length(),i;
+		String rev="";  
+	    for(i=c-1;i>=0;i--){  
+	        rev+=a.charAt(i);  
+	    }  
+	      
+		System.out.println(rev);
+
+	}
+
+}
